484dade4a90f86052f878015df835a60e2283d15
Lao-Liu233 <laoliu735@gmail.com>
2022-05-10 15:10:44 +0800

    Initial Project
    Initial Project.

---
+++ src/main/java/icu/laoliu/greenhousemonitor/GreenhouseMonitorApplication.java
@@ -0,0 +1,13 @@
+package icu.laoliu.greenhousemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenhouseMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenhouseMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/icu/laoliu/greenhousemonitor/GreenhouseMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.laoliu.greenhousemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenhouseMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
