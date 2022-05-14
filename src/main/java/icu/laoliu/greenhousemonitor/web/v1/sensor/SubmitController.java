d4759ebf299d01e0148311b1027631252f7fe6d0
Lao-Liu233 <52364111+Lao-Liu233@users.noreply.github.com>
2022-05-14 02:03:13 +0000

    Add: SubmitController

---
+++ src/main/java/icu/laoliu/greenhousemonitor/web/v1/sensor/SubmitController.java
@@ -0,0 +1,12 @@
+package icu.laoliu.greenhousemonitor.web.v1.sensor;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SubmitController {
+    @GetMapping("/v1/sensor/submit")
+    public void index(String sensorid, float humidity, float temperature) {
+        System.out.println("SensorID:" + sensorid + "; Humidity:" + humidity + "; Temperature:" + temperature);
+    }
+}
