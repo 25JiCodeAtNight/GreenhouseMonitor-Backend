32057e63533cf0f632616ac0e7064269bfcd3100
FishinTroubledWater <101721304+FishinTroubledWater@users.noreply.github.com>
2022-06-22 20:11:51 +0800

    detail文件夹移到v1下

---
+++ src/main/java/icu/laoliu/greenhousemonitor/web/v1/greenhouse/data/DetailRespondData.java
@@ -0,0 +1,11 @@
+package icu.laoliu.greenhousemonitor.web.v1.greenhouse.data;
+
+public class DetailRespondData {
+    public String name;
+    public float temp;
+    public float humi;
+    public String weather;
+    public int cloud;
+    public int windspeed;
+    public int pressure;
+}
