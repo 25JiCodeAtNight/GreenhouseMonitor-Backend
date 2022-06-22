e3255879cb0793acfbd1f678f5bc2664d2558deb
Lao_Liu <laoliu735@gmail.com>
2022-06-22 19:56:26 +0800

    Add Greenhouse Get Detail Information API
    v1/greenhouse/detai
    with test data

---
+++ src/main/java/icu/laoliu/greenhousemonitor/greenhouse/data/DetailRespondData.java
@@ -0,0 +1,11 @@
+package icu.laoliu.greenhousemonitor.greenhouse.data;
+
+public class DetailRespondData {
+    public String name;
+    public float temp;
+    public float humi;
+    public String weather;
+    public int cloud;
+    public int windspeed;
+    public int pressure;
+}
