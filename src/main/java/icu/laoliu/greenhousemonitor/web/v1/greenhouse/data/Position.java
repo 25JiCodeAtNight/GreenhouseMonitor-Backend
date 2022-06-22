49db8cbdbfe8311c48c672b60911c657f20d5b56
fzuxpc <95233598+fzuxpc@users.noreply.github.com>
2022-06-23 00:10:44 +0800

    注册大棚

---
+++ src/main/java/icu/laoliu/greenhousemonitor/web/v1/greenhouse/data/Position.java
@@ -0,0 +1,7 @@
+package icu.laoliu.greenhousemonitor.web.v1.greenhouse.data;
+
+public class Position{
+    public float latitude;
+    public float longitude;
+}
+
---
+++ src/main/java/icu/laoliu/greenhousemonitor/web/v1/greenhouse/data/RegisterRespondData.java
@@ -0,0 +1,9 @@
+package icu.laoliu.greenhousemonitor.web.v1.greenhouse.data;
+
+public class RegisterRespondData {
+    public String name;
+    public String userId;
+    public Position position;
+
+
+}
