d9f7990c1074e5cb9e177c28f3927dc6d260a7e2
Lao_Liu <laoliu735@gmail.com>
2022-06-24 03:24:51 +0800

    Update: getAllSensors API

--- src/main/java/icu/laoliu/greenhousemonitor/web/v1/user/getAllSensors.java
+++ src/main/java/icu/laoliu/greenhousemonitor/web/v1/user/getAllSensors.java
@@ -1,8 +1,6 @@
 package icu.laoliu.greenhousemonitor.web.v1.user;
 
-
 import com.google.gson.Gson;
-import icu.laoliu.greenhousemonitor.DB.DataBase;
 import icu.laoliu.greenhousemonitor.web.v1.user.data.GetSRecord;
 import icu.laoliu.greenhousemonitor.web.v1.user.data.GetSRespond;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +8,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
@@ -22,20 +18,15 @@ public class getAllSensors {
     String sql;
 
     @GetMapping("/v1/user/getAllSensors")
-    public String Send() {
-//        GetSRespond respond=new GetSRespond();
-//        DataBase dataBase =new DataBase();
-//        dataBase.setSQLString("SELECT name,sensor_id FROM user,greenhouse,sensor WHERE user.user_id=greenhouse.user_id AND greenhouse.greenhouse_id=sensor.greenhouse_id");
-//        ResultSet resultSet= dataBase.haveReturnExecute();
-
-        sql = "SELECT user.name,sensor_id FROM user,greenhouse,sensor WHERE user.user_id=greenhouse.user_id AND greenhouse.greenhouse_id=sensor.greenhouse_id";
+    public String Send(String userID) {
+        sql = "SELECT sensor.name,sensor_id FROM user,greenhouse,sensor WHERE user.user_id=greenhouse.user_id AND greenhouse.greenhouse_id=sensor.greenhouse_id AND user.user_id=" + "'" + userID + "'";
         List<Map<String, Object>> res = jdbcTemplate.queryForList(sql);
         GetSRespond respond = new GetSRespond();
         respond.responds = new GetSRecord[res.size()];
         for (int i = 0; i != res.size(); i++) {
             GetSRecord tmp = new GetSRecord();
             tmp.sensorID = (String) res.get(i).get("sensor_id");
-            tmp.name = (String) res.get(i).get("name");
+            tmp.sensorName = (String) res.get(i).get("name");
             respond.responds[i] = tmp;
         }
 
