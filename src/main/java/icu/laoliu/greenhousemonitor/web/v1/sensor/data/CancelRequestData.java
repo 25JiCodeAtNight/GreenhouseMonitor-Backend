bd41e7ed05f389e76ecfe990a7c6fafc0df556c1
Lao_Liu <laoliu735@gmail.com>
2022-06-24 03:36:38 +0800

    Update: sensorCancel API

--- src/main/java/icu/laoliu/greenhousemonitor/web/v1/sensor/data/CancelRequestData.java
+++ src/main/java/icu/laoliu/greenhousemonitor/web/v1/sensor/data/CancelRequestData.java
@@ -1,6 +1,6 @@
 package icu.laoliu.greenhousemonitor.web.v1.sensor.data;
 
 public class CancelRequestData {
-    public String greenhouseID;
+    public String userID;
     public String sensorID;
 }
