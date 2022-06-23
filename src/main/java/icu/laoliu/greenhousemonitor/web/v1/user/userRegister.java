70c6771daa3fbaa1b43c4d42d7c3e7ec4a42e3d3
Lao_Liu <laoliu735@gmail.com>
2022-06-24 02:31:15 +0800

    Update userRegister.java

--- src/main/java/icu/laoliu/greenhousemonitor/web/v1/user/userRegister.java
+++ src/main/java/icu/laoliu/greenhousemonitor/web/v1/user/userRegister.java
@@ -2,10 +2,9 @@ package icu.laoliu.greenhousemonitor.web.v1.user;
 
 import com.google.gson.Gson;
 import icu.laoliu.greenhousemonitor.web.v1.user.data.RegisterRequest;
-import icu.laoliu.greenhousemonitor.web.v1.user.data.RegisterRespond;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,34 +17,12 @@ public class userRegister {
     String sql;
     String UID = new String();
 
-    @GetMapping("/v1/user/userRegister")
-    public String Get(@RequestBody RegisterRequest request) {
-        /*在数据库建立用户记录，并返回userID*/
-//        UUID uuid=new UUID(20,4);
-//        uuid = UUID.fromString(request.openID);
-//        UID = uuid.toString();
-//        sql = "" + greenhouseid;
+    @PostMapping("/v1/user/userRegister")
+    public String Get(@RequestBody String data) {
+        // 解析请求体
+        RegisterRequest request = new Gson().fromJson(data, RegisterRequest.class);
         String UID = UUID.randomUUID().toString().replaceAll("-", "");
         jdbcTemplate.update("INSERT INTO user(user_id,name,open_id) VALUES('" + UID + "','" + request.name + "','" + request.openID + "')");
-        RegisterRespond respond = new RegisterRespond();
-        respond.userID = UID;
-        String dataJson = new Gson().toJson(respond);
-        return dataJson;
+        return UID;
     }
-    /*@GetMapping("/v1/user/register")
-    public String Send() {
-        RegisterRespond respond = new RegisterRespond();
-        respond.userID = UID;
-        String dataJson = new Gson().toJson(respond);
-        return dataJson;
-    }*/
-
-//    @GetMapping("/v1/user/register")
-//    public String Send() {
-//        RegisterRespond respond = new RegisterRespond();
-//        respond.userID = UID;
-//        String dataJson = new Gson().toJson(respond);
-//        return dataJson;
-//    }
-
 }
