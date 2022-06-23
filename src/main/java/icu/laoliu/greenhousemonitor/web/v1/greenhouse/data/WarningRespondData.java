1561829527de27abc291fcbb46b73248449861d9
Kat911 <623782192@qq.com>
2022-06-24 04:25:13 +0800

    warning

--- src/main/java/icu/laoliu/greenhousemonitor/web/v1/greenhouse/data/WarningRespondData.java
+++ src/main/java/icu/laoliu/greenhousemonitor/web/v1/greenhouse/data/WarningRespondData.java
@@ -3,8 +3,15 @@ package icu.laoliu.greenhousemonitor.web.v1.greenhouse.data;
 public class WarningRespondData {
     public WarningData[] warningData;
 
+    public  WarningRespondData(int a){
+        warningData = new WarningData[a];
+        for (int i = 0; i < a; i++) {
+            warningData[i] = new WarningData();
+        }
+    }
+
     public class WarningData {
-        public int id;
+        public String id;
         public String name;
     }
 }
--- src/main/java/icu/laoliu/greenhousemonitor/web/v1/greenhouse/warning.java
+++ src/main/java/icu/laoliu/greenhousemonitor/web/v1/greenhouse/warning.java
@@ -18,24 +18,31 @@ public class warning {
     String sql;
 
     @GetMapping("v1/greenhouse/warning")
+
     public String index(String greenhouseid) {
+
         sql = "SELECT latitude,longitude FROM greenhouse WHERE greenhouse_id=" + greenhouseid;
         Map<String, Object> result = jdbcTemplate.queryForMap(sql);
         float latitude = 0, longitude = 0;
         latitude = (float) result.get("latitude");
         longitude = (float) result.get("longitude");
 
+
         //创建WeatherAPI
         WeatherAPI weatherAPI = new WeatherAPI("TOKEN", latitude + "," + longitude);
-        DisasterWarningData disasterWarningData = weatherAPI.getDisasterWarning();
+
+
 
         //创建WarningRespondData
-        WarningRespondData warningRespondData = new WarningRespondData();
-        warningRespondData.warningData = new WarningRespondData.WarningData[disasterWarningData.warning.length];
-        for (int i = 0; i < disasterWarningData.warning.length; i++) {
-            warningRespondData.warningData[i] = warningRespondData.new WarningData();
-            warningRespondData.warningData[i].id = disasterWarningData.code;
-            warningRespondData.warningData[i].name = disasterWarningData.warning[i];
+        WarningRespondData warningRespondData = new WarningRespondData(weatherAPI.getDisasterWarning().warning.length);
+
+
+
+        for (int i = 0; i < weatherAPI.getDisasterWarning().warning.length; i++) {
+          warningRespondData.warningData[i].id = (weatherAPI.getDisasterWarning().warning[i].id);
+          warningRespondData.warningData[i].name = weatherAPI.getDisasterWarning().warning[i].title;
+
+
         }
 
         String dataJson = new Gson().toJson(warningRespondData);
